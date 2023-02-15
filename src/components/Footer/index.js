5d255bc795167ba3798bd596b12dd345cfb43416
Vivek kumar <vivekkumar@Viveks-MacBook-Pro.local>
2023-02-15 18:08:53 +0530

    final push

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,12 +8,12 @@ const Footer = ({bottom}) =>{
         <Wrapper>
             <Content className={bottom} >
                 <div>
-                <a href="https://github.com/chirag-23/Tesla-clone-reactjs" target='_blank' >Tesla-Clone &copy; 2022</a>
+                <a href="https://github.com/Vivek4551/tesla-clone" target='_blank' >Tesla-Clone &copy; 2022</a>
                 </div>
                 <div>
                 <a href="mailto:chiragchouhan163@gmail.com" target='_blank'>Gmail</a>
-                <a href="https://www.linkedin.com/in/chirag-chouhan-b48311228/" target="_blank" >LinkedIn</a>
-                <a href="https://github.com/chirag-23" target="_blank" >GitHub</a>
+                <a href="https://www.linkedin.com/in/vivek-kumar-5a0b38200/" target="_blank" >LinkedIn</a>
+                <a href="https://github.com/Vivek4551" target="_blank" >GitHub</a>
                 </div>
             </Content>
         </Wrapper>
--- src/components/Model-S/index.js
+++ src/components/Model-S/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { Wrapper,Content } from "./models.style";
-
 import Section from "../Section";
 import Interior from "../Interior_Specs";
 import Specs from "../Gen_Spec";
